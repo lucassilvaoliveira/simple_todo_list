0c9d4aa221bd8fa5d09fcf76401b2df0f7da236a
dev-lucas <silvaluxas5@gmail.com>
2023-02-24 10:40:03 -0300

    setup folders

---
+++ lib/app/todo_solve_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo_solve_list/app/screens/main/main_screen.dart';
+
+class TodoSolveList extends StatelessWidget {
+  const TodoSolveList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
